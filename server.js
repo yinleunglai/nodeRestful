1c5ea0d7702067ac834409865e44eb29205f1759
Simon Li1 <simon.li1@ihg.com>
2017-11-29 15:25:38 +0800

    初始化项目

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3001,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'),
+    bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/todolist'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+app.listen(port);
+
+console.log('todo list restful api server start on: ' + port);
